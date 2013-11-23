fd510aee4d86633ddb08a05814ca723aa7e28100
Bojan Glođaić <glbojan@gmail.com>
2013-11-23 11:24:40 +0100

    initial commit

---
+++ digi-clock.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.clock-wrapper').dclock({
+        fontSize: 144,
+        fontColor: '#271069',
+        backColor: '#E0E0E0',
+        padding: 1,
+        // fontFamily: 'Arial',
+    });
+});
